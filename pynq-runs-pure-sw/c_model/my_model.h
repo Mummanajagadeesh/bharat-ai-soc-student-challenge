d9a0dbc664b38ad9b47ae7aa6c743d8280d447a1
Mummanajagadeesh <mummanajagadeesh97@gmail.com>
2026-02-20 19:05:18 +0530

    chore: removed c_model

--- pynq-runs-pure-sw/c_model/my_model.h
+++
@@ -1,5 +0,0 @@
-#pragma once 
-#include "./include/k2c_tensor_include.h" 
-void my_model(k2c_tensor* input_layer_input, k2c_tensor* keras_tensor_25_output); 
-void my_model_initialize(); 
-void my_model_terminate(); 
